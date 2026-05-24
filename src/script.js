0ce999d878d593ff429fa6531809b359e93567fa
menali17 <enzomenali@gmail.com>
2026-05-24 13:58:40 -0300

    Adiciona código do desafio e estiliza CSS

--- src/script.js
+++ src/script.js
@@ -1,23 +1,44 @@
-//função de mudar imagem pelo id e pela url
 function changeImage(id, url) {
   document.getElementById(id).src = url;
 }
-//função de mudar texto pelo id e pelo texto
 function changeText(id, text) {
   document.getElementById(id).innerText = text;
 }
 
-// Daqui para baixo voce ira escrever
-// o código para resolver o desafio
+let pokemon = [];
+let pokemonIndex = 0;
 
+async function loadPokemonList() {
+  const response = await fetch("https://pokeapi.co/api/v2/pokemon/?offset=0&limit=1292");
+  const data = await response.json();
+  pokemon = data.results;
+  showPokemon();
+}
+
+async function showPokemon() {
+  const pokemonAtual = pokemon[pokemonIndex];
+  const response = await fetch(pokemonAtual.url);
+  const data = await response.json();
+
+  const imageUrl = data.sprites.front_default || "../assets/missingno.png";
+
+  changeText("name", data.name);
+  changeImage("img_sprite_front_default", imageUrl);
+  }
+
+loadPokemonList();
 function previousPokemon() {
-  alert("Pokemon Anterior");
-  //abra o terminal em inspecionar no chrome para visualizar
-  console.log("Pokemon Anterior");
+  pokemonIndex -= 1;
+  if (pokemonIndex < 0) {
+  pokemonIndex = pokemon.length - 1;
+  }
+  showPokemon();
 }
 
 function nextPokemon() {
-  alert("Pokemon Seguinte");
-  //abra o terminal em inspecionar no chrome para visualizar
-  console.log("Pokemon Seguinte");
+  pokemonIndex +=1;
+  if (pokemonIndex >= pokemon.length){
+    pokemonIndex = 0;
+  }
+  showPokemon();
 }
